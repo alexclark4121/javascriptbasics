8db49e36532a5eb70b6a1466dd40ceaa3861c0be
Alex Clark <alexclark4121@gmail.com>
2019-11-10 16:25:22 -0500

    structured the page, added the first example which is a button that changes the text above it

---
+++ scripts.js
@@ -0,0 +1,4 @@
+function buttoncreate() {
+    document.getElementById('paragraph').innerHTML = "OK, well you clicked but while we are here this happened as a reusult of calling a function on this button by clicking it.  It traverses the DOM to to find the element you specifiy and a function that changes the text on the page."
+}
+
