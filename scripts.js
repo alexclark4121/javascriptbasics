9ae22aba8c3ed794bb2ea3378803d1c3bba80eb4
Alex Clark <alexclark4121@gmail.com>
2019-11-11 17:13:20 -0500

    Added alert button example

--- scripts.js
+++ scripts.js
@@ -28,6 +28,16 @@ function backgroundImage() {
     document.body.style.backgroundImage = "url('http://blogs.discovermagazine.com/d-brief/files/2019/02/Ocean-Blue.jpg')";
 }
 
+
+ /*You can't change the background to and image and then 
+ turn it back to a color unless you reset the background
+ with "initial" which sets it to the default way it is 
+ set when you first load the page */
+
 function removebackgroundimage() {
-    document.body.style.backgroundImage = "initial";
+    document.body.style.backgroundImage = "initial"; 
+}
+
+function alertbutton() {
+    alert("You have been alerted");
 }
